47c1e16602ba35a48ccccdc68f815e9e3cba9a4c
Daniel Thompson <dthompson0190@gmail.com>
2021-11-12 00:44:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './media/tracker_logo.svg';
+import './css/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function colorMode(){
+		document.querySelector('.App').classList.toggle('dark')
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<button >
+					<label className="switch">
+						<input type="checkbox" onClick={colorMode}/>
+						<span className="slider round">
+							<i className="material-icons sun">brightness_7</i>
+							<i className="material-icons moon">brightness_3</i>
+						</span>
+					</label>
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
